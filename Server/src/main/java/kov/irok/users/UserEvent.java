c558534b9e3290e103d101883f32ff067c0135a6
Natallia Vasilyeva <32133652+NatalliaVasilyeva@users.noreply.github.com>
2019-06-23 22:22:17 +0300

    Add some comment to code

--- Server/src/main/java/kov/irok/users/UserEvent.java
+++ Server/src/main/java/kov/irok/users/UserEvent.java
@@ -5,13 +5,13 @@ import kov.irok.Connection;
 public interface UserEvent {
     Connection register(Connection connectionFrom, String[] strings, Connection connectionTo);
 
-    void leave(Connection connection);
-    void exit(Connection connection);
+    void leave(Connection connection); // Method is unimplemented and never used.
+    void exit(Connection connection); // Method is unimplemented and never used.
 
     default String getName(String[] strings){
         StringBuffer name = new StringBuffer();
         for (int i = 2; i < strings.length; i++){
-            name = name.append(strings[i] + " ");
+            name = name.append(strings[i] + " "); // Not required to assign value. Append is just enough.
         }
         return name.toString();
     }
